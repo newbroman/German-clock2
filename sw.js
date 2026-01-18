4cd37356a8a49916340838c700fb7d0938a4daff
newbroman <105666230+newbroman@users.noreply.github.com>
2026-01-18 00:51:22 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 1. Updated Cache Name to match your German project
-const CACHE_NAME = 'german-clock-v101'; 
+const CACHE_NAME = 'german-clock-v103'; 
 const GH_PATH = '/German-clock2'; 
 
 const ASSETS = [
